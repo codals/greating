9c96e88a5f11fe629b22b410646d391232b50110
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-05-23 20:50:25 +0900

    #3 feat: 메인화면 구현

---
+++ src/main/webapp/resources/js/main/slick.js
@@ -0,0 +1,17 @@
+ $( document ).ready( function() {
+        $( '.slider' ).slick( {
+          autoplay: true,
+          autoplaySpeed: 1000,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        } );
+      } );
+ 
+ $( document ).ready( function() {
+     $( '.event-slider' ).slick( {
+       autoplay: true,
+       autoplaySpeed: 1000,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+     } );
+   } );
