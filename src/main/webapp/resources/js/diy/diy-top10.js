da78567403b5de1ee1774ddc58c66dac2c226952
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-05-25 17:28:54 +0900

    feat: DIY 식단 top 10 페이지 구현 1차 완료
    issue; #8

---
+++ src/main/webapp/resources/js/diy/diy-top10.js
@@ -0,0 +1,9 @@
+ $( document ).ready( function() {
+        $( '.slider' ).slick( {
+          autoplay: true,
+          autoplaySpeed: 2000,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        } );
+      } );
+ 
