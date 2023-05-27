c9c451c073e3806a2d5142873027197cce642152
sa46lll <azure394@gmail.com>
2023-05-27 23:53:01 +0900

    chore: 파일 경로 이동 및 불필요한 임포트문 삭제

--- src/main/java/com/codals/greating/diy/controller/DiyController.java
+++ src/main/java/com/codals/greating/diy/controller/DiyController.java
@@ -1,12 +1,10 @@
-package com.codals.greating.diy;
+package com.codals.greating.diy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/mealdiy")
