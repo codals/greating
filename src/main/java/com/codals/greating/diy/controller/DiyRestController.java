bfa8c151e3ecb5112b7d3b4a6f236691c1c4e976
송민진 <m_6595@naver.com>
2023-05-30 14:13:00 +0900

    fix: Dao -> DAO로 이름 변경
    issue: #53

--- src/main/java/com/codals/greating/diy/controller/DiyRestController.java
+++ src/main/java/com/codals/greating/diy/controller/DiyRestController.java
@@ -1,19 +1,28 @@
 package com.codals.greating.diy.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.codals.greating.diy.dto.DiyRequestDto;
+import com.codals.greating.diy.service.DiyService;
+import com.codals.greating.user.entity.User;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/mealdiy")
 public class DiyRestController {
+	
+	private final DiyService diyService;
 
     @PostMapping
     public String search(@RequestParam(required = false) String category,
@@ -26,13 +35,19 @@ public class DiyRestController {
         return "diy/diy-main";
     }
 
-    @PostMapping("/new")
-    public String createNewPost(/* @SessionAttribute("loginUser") User loginUser, */
-                                @ModelAttribute DiyRequestDto newPost) {
-    	
-        /* log.info(loginUser); */
-        log.info(newPost);
-        
-        return "/greating/mealdiy/" + "1";
-    }
+	@PostMapping("/new")
+	public String createPost(@SessionAttribute("loginUser") User loginUser,
+								@ModelAttribute DiyRequestDto newPost,
+								HttpSession session) {
+
+		/* log.info(loginUser); */
+		log.info(newPost);
+
+		// 톰캣 아래 바로 이미지 넣는 경로
+		String path = session.getServletContext().getRealPath("/") + "resources/images";
+
+		diyService.createPost(newPost, path);
+
+		return "/greating/mealdiy/" + "1";
+	}
 }
