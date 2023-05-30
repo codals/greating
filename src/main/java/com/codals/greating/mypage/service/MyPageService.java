6ae9d0a8c806a436cf7d42ce39bd9b7dd56bb284
tigre911 <tmvhswlqkqn_@naver.com>
2023-05-30 19:33:17 +0900

    feat: 마이페이지 내가 스크랩한 식단 기능추가
    issue: #68

--- src/main/java/com/codals/greating/mypage/service/MyPageService.java
+++ src/main/java/com/codals/greating/mypage/service/MyPageService.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.codals.greating.mypage.dto.MyPageDto;
+import com.codals.greating.mypage.dto.MyPageScrapDto;
 
 
 public interface MyPageService {
 
 	public List<MyPageDto> diyList(MyPageDto dto);
+
+	public List<MyPageDto> scrapList(MyPageScrapDto dto);
 
 }
