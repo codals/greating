854eaaf236e92f09f1d5de2c11efbd96ab509615
SeungJoo❤️ <seoungjoo01@gmail.com>
2023-06-05 19:03:28 +0900

    feat: Redis 설치 및 연결
    issue: #137

---
+++ src/main/java/com/codals/greating/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.codals.greating.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+	
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName("127.0.0.1");
+		redisStandaloneConfiguration.setPort(6379);
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+		return lettuceConnectionFactory;
+	}
+	
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate(){
+		RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		return redisTemplate;
+	}	 	
+}
