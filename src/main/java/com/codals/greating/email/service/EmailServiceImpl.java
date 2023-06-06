094ece023f5849a1945768707099f257bdd04422
sa46lll <9812smh@naver.com>
2023-06-06 20:59:01 +0900

    feat: 이메일 비동기 처리
    issue: #140

--- src/main/java/com/codals/greating/email/service/EmailServiceImpl.java
+++ src/main/java/com/codals/greating/email/service/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.codals.greating.email.service;
 import com.codals.greating.external.sender.GmailEmailSender;
 import com.codals.greating.email.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 
@@ -13,6 +14,8 @@ public class EmailServiceImpl implements EmailService {
     private final GmailEmailSender gmailEmailSender;
     private static final String SUBJECT_ORDER_COMPLETED = "주문 확인 및 배송 안내";
 
+    @Async
+    @Override
     public void sendOrderEmail(OrderDto order) {
         Context context = new Context();
         context.setVariable("name", order.getUserName());
