cf39e3a6a007c832f4e8ae14add925d6886a3f36
sa46lll <9812smh@naver.com>
2023-06-06 16:05:26 +0900

    feat: javax.mail 세팅
    issue: #140

---
+++ src/main/java/com/codals/greating/email/MailConfig.java
@@ -0,0 +1,33 @@
+package com.codals.greating.email;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername("gmailUsername");
+        javaMailSender.setPassword("gmailPassword");
+        javaMailSender.setPort(587);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+    }
+}
